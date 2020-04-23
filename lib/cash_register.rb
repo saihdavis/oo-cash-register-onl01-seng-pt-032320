564d7cef551041dc7b1aa47e7263933e127bc09d
Saida Davis <saihdavis@gmail.com>
2020-04-23 05:11:42 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
-
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0)
+    @total= total
+  end  
+  
+  def discount
+   # binding.pry
+    self.total
+end
+end
